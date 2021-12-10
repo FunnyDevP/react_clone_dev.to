4a2581e4773c0d8a66ca4cc65b22a20166fcaa66
FunnyDevP <Waranchit.ch@outlook.com>
2021-12-10 20:48:58 +0700

    custom font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,18 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Roboto Mono", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   variants: {
-    extend: {
-      fontFamily: {},
-    },
+    extend: {},
   },
   plugins: [],
 };
